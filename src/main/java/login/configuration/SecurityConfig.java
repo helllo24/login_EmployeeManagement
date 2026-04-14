84e790f42e7121099b7d9b0c0c7525c14492aaf2
sudhakar <jsudhakar2415@gmail.com>
2026-04-14 22:52:27 +0530

    chenge cros versel

--- src/main/java/login/configuration/SecurityConfig.java
+++ src/main/java/login/configuration/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
 
         config.setAllowedOrigins(java.util.List.of("http://localhost:5500",
-                "https://helllo24.github.io/Login_FrontendProject"));
+                "https://login-frontend-project.vercel.app"));
 
         config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTION"));
         config.setAllowedHeaders(java.util.List.of("*"));
